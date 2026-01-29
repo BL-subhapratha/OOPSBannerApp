5dbc0a677ca4bd4464433b1228c6865602df957f
BL-subhapratha <subhapratha.sivaramakrishnan@bridgelabz.com>
2026-01-29 06:42:39 +0000

    feat:[Subha]-coded to print Banner OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,16 @@
 public class OOPSBannerApp {
     public static void main(String[] args){
+
+        // UC1: Print OOPS to console
         System.out.println("OOPS");
+
+        // UC2: Print multiple lines of * and spaces
+        System.out.println("  *****   " + "  *****   " + "  *****   " + "  ******  ");
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *       ");
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *       ");
+        System.out.println(" *     *  " + " *     *  " + "  *****   " + "  ******  ");
+        System.out.println(" *     *  " + " *     *  " + " *        " + "       *  ");
+        System.out.println(" *     *  " + " *     *  " + " *        " + "       *  ");
+        System.out.println("  *****   " + "  *****   " + " *        " + "  ******  ");
     }
 }
